e518af830acd929f1eee40b3a50823370d1a5688
Alisher Aliev <alisher.aliev@tietoevry.com>
2022-06-14 19:42:39 +0300

    Initialized the assigment project with NodeJS on TypeScript

---
+++ .eslintrc.js
@@ -0,0 +1,60 @@
+module.exports = {
+    'env': {
+        'commonjs': true,
+        'es2021': true,
+        'node': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking'
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'project': './tsconfig.json'
+    },
+    'plugins': [
+        '@typescript-eslint'
+    ],
+    'rules': {
+        '@typescript-eslint/semi': ['error'],
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/restrict-template-expressions': 'off',
+        '@typescript-eslint/restrict-plus-operands': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          { 'argsIgnorePattern': '^_' }
+        ],
+        'no-case-declarations': 'off',
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'eol-last': [
+          'error', 'always'
+        ],
+        'eqeqeq': 'error',
+        'no-trailing-spaces': 'error',
+        'object-curly-spacing': [
+          'error', 'always'
+        ],
+        'arrow-spacing': [
+          'error', { 'before': true, 'after': true }
+        ],
+        'no-console': 0,
+    }
+};
